4c86e6361b443bcbe03895546ed2072511549937
Peter Cho <pcho@phase2technology.com>
2016-05-11 10:53:53 -0400

    Clean up

--- gulpfile.js
+++ gulpfile.js
@@ -8,37 +8,41 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 
-// Lint Task
+// Lint Tasks
+// > gulp lint
 gulp.task('lint', function() {
-    return gulp.src('js/*.js')
-        .pipe(jshint())
-        .pipe(jshint.reporter('default'));
+  return gulp.src('js/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
 });
 
 // Compile Our Sass
+// > gulp sass
 gulp.task('sass', function() {
-    return gulp.src('scss/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('dist/css'));
+  return gulp.src('scss/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('dist/css'));
 });
 
 // Watch Files For Changes
+// > gulp watch
 gulp.task('watch', function() {
-    gulp.watch('js/*.js', ['lint', 'scripts']);
-    gulp.watch('components/*.jsx', ['lint', 'scripts']);
-    gulp.watch('scss/*.scss', ['sass']);
+  gulp.watch('js/*.js', ['lint', 'scripts']);
+  gulp.watch('components/*.jsx', ['lint', 'scripts']);
+  gulp.watch('scss/*.scss', ['sass']);
 });
 
 // Concatenate & Minify JS
+// > gulp scripts
 gulp.task('scripts', function() {
     return gulp.src('js/*.js')
-        .pipe(concat('all.js'))
-        .pipe(gulp.dest('dist'))
-        .pipe(rename('all.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('dist/js'));
+      .pipe(concat('all.js'))
+      .pipe(gulp.dest('dist'))
+      .pipe(rename('all.min.js'))
+      .pipe(uglify())
+      .pipe(gulp.dest('dist/js'));
 });
 
-// Default Task
+// Default Tasks
+// > gulp
 gulp.task('default', ['lint', 'sass', 'scripts', 'watch']);
-
