b18be4f720a41c9870244460c368d6d836d5430e
Peter Cho <pcho@phase2technology.com>
2016-05-16 12:57:38 -0400

    Removed z

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,10 +5,10 @@ const TemplateDefault = require('../components/20-templates/default.jsx');
 
 const Index = React.createClass({
   render: function() {
-    let contentz = <strong>42qfrv</strong>;
+    let content = <strong>42qfrv</strong>;
 
     return (
-      <TemplateDefault content={contentz} />
+      <TemplateDefault content={content} />
     );
   }
 });
