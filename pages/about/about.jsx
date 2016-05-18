caa4ec26dfc2f86db76bf189dde21b8803da66e3
Peter Cho <pcho@phase2technology.com>
2016-05-18 10:47:04 -0400

    Put in conditional to allow for proper SSR

--- pages/about/about.jsx
+++ pages/about/about.jsx
@@ -1,7 +1,8 @@
 'use strict';
 
 const React = require('react'),
-      ReactDOM = require('react-dom');
+      ReactDOM = require('react-dom'),
+      ExecutionEnvironment = require('exenv');
 const TemplateDefault = require('../../components/20-templates/default.jsx');
 
 const About = React.createClass({
@@ -13,5 +14,11 @@ const About = React.createClass({
     );
   }
 });
+
+if (ExecutionEnvironment.canUseDOM) {
+  ReactDOM.render((
+    <About />
+  ), document.html);
+}
 
 module.exports = About;
