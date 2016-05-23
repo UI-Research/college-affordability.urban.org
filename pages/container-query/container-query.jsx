89f90a148217faee0c04d73616b1c504a7404eb3
Peter Cho <pcho@phase2technology.com>
2016-05-23 13:44:07 -0400

    Fixed linting errors

--- pages/container-query/container-query.jsx
+++ pages/container-query/container-query.jsx
@@ -1,7 +1,6 @@
 'use strict';
 
-const React = require('react'),
-      ReactDOM = require('react-dom');
+const React = require('react');
 const TemplateDefault = require('../../components/20-templates/default/default.jsx');
 
 const ContainerPage = React.createClass({
@@ -15,7 +14,7 @@ const ContainerPage = React.createClass({
     );
 
     let title = ('React Container Demo'),
-      includeHTML = true;
+        includeHTML = true;
 
     return (
       <TemplateDefault content={content} title={title} includeHTML={includeHTML} />
