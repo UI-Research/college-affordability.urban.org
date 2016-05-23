0f7579c51fbc078b67c2d821c1831e1592ed9f99
Peter Cho <pcho@phase2technology.com>
2016-05-23 00:09:42 -0400

    Semi-finalized implementation

---
+++ components/global.jsx
@@ -0,0 +1,5 @@
+'use strict';
+
+module.exports = {
+  'components': '../../components/30-components'
+};
--- pages/about/about.jsx
+++ pages/about/about.jsx
@@ -3,27 +3,36 @@
 const React = require('react'),
       ReactDOM = require('react-dom'),
       ExecutionEnvironment = require('exenv');
-const TemplateDefault = require('../../components/20-templates/default.jsx');
+const Template = require('../../components/20-templates/default.jsx');
+const global = require('../../components/global.jsx');
+const Text = require(global.components + '/basic/text/text.jsx');
 
+let specifications = {
+  'name': 'about',
+  'content': <div className='test'>
+        <Text title="Law & Order">
+          <p>What are you doing?</p>
+          <p>I don't like</p>
+        </Text>
+        <div id='asdf'><strong>About US !!!</strong></div>
+      </div>
+};
 
-const Derp = require('./derp.js');
-
-const About = React.createClass({
+const Page = React.createClass({
   render: function() {
-
     return (
-      <TemplateDefault title="About USA" includeHTML={this.props.includeHTML}>
-        {Derp.content}
-      </TemplateDefault>
+      <Template title="About USA" includeHTML={this.props.includeHTML}>
+        {specifications.content}
+      </Template>
     );
   }
 });
 
 if (ExecutionEnvironment.canUseDOM) {
-  require('./about.scss');
+  require('./' + specifications.name.toLowerCase() + '.scss');
   ReactDOM.render((
-    <About />
+    <Page />
   ), document.getElementById('container'));
 }
 
-module.exports = About;
+module.exports = Page;
