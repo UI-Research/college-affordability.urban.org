56773ee94aa1fe820e87bb6ee55f78905344c952
Joey Groh <jgroh@phase2technology.com>
2016-05-24 11:14:34 -0700

    Adjusting colors, html for header, css for nav

--- components/20-templates/default/components/header.jsx
+++ components/20-templates/default/components/header.jsx
@@ -4,12 +4,35 @@ const React = require('react'),
 
 if (ExecutionEnvironment.canUseDOM) {
   require('./header.scss');
+  require('./nav-primary.scss');
 }
 
 const Header = React.createClass({
   render: function() {
     return (
-      <header className="header-site">This is a header</header>
+      <header className="header-site">
+        <a className="header-site__logo" alt="Urban Institute">Title</a>
+        <div className="header-site__nav">
+          <div className="nav-primary">
+            <ul>
+              <li>
+                <a href="#">Item 1</a>
+                <ul>
+                  <li>
+                    <a href="#">Sub Item</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a href="#">Item 2</a>
+              </li>
+              <li>
+                <a href="#">Item 2</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </header>
     );
   }
 });
