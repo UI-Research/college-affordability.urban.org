59c80fabaec169b87ab900d4693323062e182dd8
Peter Cho <pcho@phase2technology.com>
2016-06-22 14:23:32 -0400

    Moved test page to better labeled directory, created official about page

--- pages/about/about.jsx
+++ pages/about/about.jsx
@@ -9,134 +9,24 @@ const Text = require('30-components/basic/text/text.jsx'),
 
 const util = require('util.jsx');
 
-let multipage = {
-  links: [
-    'one',
-    [
-      'uno',
-      'quatro',
-      'cinco'
-    ],
-    'two',
-    [
-      'dos',
-      'tres',
-      'seis',
-      'siete',
-      'ocho'
-    ],
-    'three'
-  ],
-  content: {
-    'one': {
-      title: 'ONE',
-      content: (
-        <div>
-          <div className="grid">
-            <div className="col col--2-3">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut venenatis feugiat tellus, ac laoreet nulla dignissim vitae. Nullam vulputate ante in turpis rutrum semper. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Phasellus et scelerisque lectus. Vestibulum nec pretium lacus, id suscipit ligula. Praesent fermentum, sem eu hendrerit ultricies, metus odio luctus urna, ut lacinia diam odio quis neque. Aliquam bibendum lorem nec rhoncus fringilla. Interdum et malesuada fames ac ante ipsum primis in faucibus. Integer cursus mi posuere, placerat lacus at, sodales nunc.
-            </div>
-            <div className="col col--1-3">Second colum</div>
-          </div>
-        </div>
-      )
-    },
-    'two': {
-      title: 'TWO',
-      content: (
-        <div>
-          <img src='/img/PSVFcxr.jpg' />
-          <BarGraph title="two" file={require('./sample.js')} />
-        </div>
-      )
-    },
-    'three': {
-      title: 'THREE',
-      content: (
-        <div>
-          <img src='/img/MPxXjTj.jpg' />
-          <BarGraph title="three" file={require('./sample.js')} />
-        </div>
-      )
-    },
-    'uno': {
-      title: 'UNO',
-      content: (
-        <div>
-          <img src='/img/EYUcKub.jpg' />
-          <BarGraph title="seven" file={require('./sample.js')} />
-        </div>
-      )
-    },
-    'dos': {
-      title: 'DOS',
-      content: (
-        <div>
-          <img src='/img/PSVFcxr.jpg' />
-          <BarGraph title="eight" file={require('./sample.js')} />
-        </div>
-      )
-    },
-    'tres': {
-      title: 'TRES',
-      content: (
-        <div>
-          <LineGraph title="My Line Chart" file={require('./sample.js')} />
-          <i className="fa fa-camera-retro"></i>
-        </div>
-      )
-    },
-    'quatro': {
-      title: 'QUATRO',
-      content: (
-        <div>
-          <Text title="General Text Box">
-            <p>Example of a text box?</p>
-            <a href="http://www.google.com">This is a link</a>
-          </Text>
-          <BarGraph title="one" anchor_name="bar_one" file={require('./sample.js')} />
-        </div>
-      )
-    },
-    'cinco': {
-      title: 'CINCO',
-      content: (
-        <div>
-          <img src='/img/MPxXjTj.jpg' />
-          <BarGraph title="My Bar Chart" file={require('./sample.js')} />
-        </div>
-      )
-    },
-    'seis': {
-      title: 'SEIS',
-      content: (
-        <div>
-          <img src='/img/4HLZjYB.jpg' />
-          <BarGraph title="five" anchor_name="five" file={require('./sample.js')} />
-        </div>
-      )
-    },
-    'siete': {
-      title: 'SIETE',
-      content: (
-        <div>
-          <LineGraph title="four" anchor_name="four_four" file={require('./sample.js')} />
-          <BarGraph title="six" anchor_name="six" file={require('./sample.js')} />
-        </div>
-      )
-    },
-    'ocho': {
-      title: 'OCHO',
-      content: (
-        <div className='asdf'><strong>Custom HTML</strong></div>
-      )
-    }
-  }
-};
 
 let specifications = {
   'name': 'about',
-  'content': <MultiPage content={multipage} />
+  'content': (
+    <div>
+      <h1>About</h1>
+      <p>Rising concerns over college tuition and student debt combined with the widespread conviction that a degree is essential for a middle-class lifestyle have led to a sense that college in the United States has become "unaffordable." <strong>But what does affordability actually mean?</strong></p>
+      <h2>Affordability depends on what you’re buying</h2>
+      <p>"College" means many different things, and whether college is "worth it depends on what kind of institution and program you’re talking about. There are thousands of postsecondary institutions in the United States offering a wide range of undergraduate credentials, from short-term certificates in very specific occupational areas to associate degrees in either general academic fields or more targeted vocational areas to bachelor’s degrees—which also come in many different forms.</p>
+      <p>Just as someone who can afford to pay $20,000 for a new car cannot afford to pay nearly that much for a television, students should think of the price of a bachelor's degree quite differently than they think of the price of a short-term certificate. They should also consider the quality of the credential and the experiences of similar students. In addition, although we almost always focus on a year's tuition when talking about college prices, the number of years for which you pay that price matters a great deal. It is much more expensive to spend five years than four years in college to earn a bachelor’s degree.</p>
+      <h2>Sticker price is not an indicator of affordability</h2>
+      <p>Most students and families don't actually pay the whole cost of education. They attend institutions that charge less in tuition and fees than the amount they spend educating students, relying on funds from state and local tax revenues, endowment income, and other philanthropic sources to cover part of the costs. Moreover, many students receive grants and tax benefits that help them cover the tuition and fees that are charged.</p>
+      <p>On the flipside, students don't just have to pay tuition and fees. They also have to cover their living expenses while they are in college. What resources are available to pay all of these bills? Students can pay their share of the costs with a combination of income before college (including income earned by their parents), income earned during college, and income earned after college. They supplement these resources with financial aid from federal and state governments, colleges and universities, and other sources.</p>
+      <h2>Affordability is about more than what you can pay right now</h2>
+      <p>Having a sense of the value of the educational experience is critical to thinking about college affordability. The benefits of college are certainly not just in the form of future earnings. Students should expect to develop personally and intellectually, to open up lots of different kinds of opportunities, and to enjoy the process. But the money matters. Even if you have all the money in your pocket for four years of tuition, the education is unaffordable if it doesn’t do enough to increase your earnings potential.</p>
+      <p><strong><em>There is not a yes-or-no answer to the question of whether or not college is affordable.</em></strong>  But the information on this website can increase understanding of how much students in different circumstances pay for different kinds of education and of the resources they can draw on to cover their expenses. By presenting the big picture, trends over time, and the option to look more closely at the details, this website should support a more coherent and nuanced conversation about students’ financial access to a college education worth its cost.</p>
+    </div>
+  )
 };
 
 const About = React.createClass({
