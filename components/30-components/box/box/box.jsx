4ca35f0a09399c1a2dfc0ce2aa53f2051a0b42a3
Ryan Smylski <ryan.smylski@gmail.com>
2016-06-23 18:19:21 -0700

    [UICA-71] - Added new react functions to capture prop changes when a box doesn't unmount/mount when pages change.

--- components/30-components/box/box/box.jsx
+++ components/30-components/box/box/box.jsx
@@ -1,5 +1,6 @@
 'use strict';
-const React = require('react');
+const React = require('react'),
+      ReactDom = require('react-dom');
 const util = require('util.jsx');
 
 if (util.canUseDOM()) {
@@ -16,64 +17,58 @@ const Box = React.createClass({
     };
   },
   componentWillMount() {
-    // Create unique ID for element.
-    this.id = 'box-' + util.uniqueID();
     this.setState({
       expanded: true,
-      showToggle: true
+      showToggle: true,
+      reload: false
     });
   },
   componentDidMount: function() {
-    // Keep track of the actual rendered height.
-    let box = document.getElementById(this.id);
-    let children = box.getElementsByClassName('inner');
-    if (children.length) {
-      let showToggle = children[0].offsetHeight > 250;
+    this._initializeBox();
+  },
+  componentWillReceiveProps: function (nextProps) {
+    // If we got new props, reset states and force reload.
+    if (!util.isEqual(this.props, nextProps)) {
       this.setState({
-        height: children[0].offsetHeight,
-        showToggle: showToggle
+        reload: true,
+        expanded: true,
+        showToggle: true,
       });
-      // Toggle the state to be collapsed.
-      this._toggleBox();
-      this.render();
     }
   },
-  render: function() {
-    let anchor = '';
-    if (this.props.anchor_name) {
-      // Replace any spaces with _.
-      let anchor_name = util.cleanString(this.props.anchor_name);
-      anchor = <a name={anchor_name}></a>;
+  componentDidUpdate: function (prevProps, prevState) {
+    // If we're set to reload, intialize for the new content.
+    if (this.state.reload) {
+      this._initializeBox();
     }
-    let toggleClass = 'c-text__box';
-    let readMore = null;
-    if (this.state.showToggle) {
-      toggleClass += this.state.expanded ? ' expanded' : ' collapsed';
-      let toggleText = this.state.expanded ? '-' : '+';
-      readMore = <div className="read-more"><a href="#" onClick={this._handleClick} className="toggle">{toggleText}</a></div>;
-    }
-    
-    return (
-      <div id={this.id} className={toggleClass}>
-        {anchor}
-        <div className="inner">
-          {this.props.children}
-        </div>
-        {readMore}
-      </div>
-    );
   },
   _handleClick: function (e) {
     e.preventDefault();
     this._toggleBox();
   },
+  // Initialize our states and start collapsed if needed.
+  _initializeBox() {
+    let box = this.refs.box;
+    let children = box.getElementsByClassName('inner');
+    if (children.length) {
+      let showToggle = children[0].offsetHeight > 250;
+      this.setState({
+        expanded: true,
+        height: children[0].offsetHeight,
+        showToggle: showToggle,
+        reload: false
+      });
+      // Toggle the state to be collapsed.
+      this._toggleBox();
+    }
+  },
   // This is its own function so we can invoke with a dom event.
   _toggleBox() {
-    let box = document.getElementById(this.id);
+    let box = this.refs.box;
     let children = box.getElementsByClassName('inner');
     // Set to the real height when toggling to expanded.
     if (!this.state.expanded && children.length) {
-      children[0].style.height = this.state.height;
+      children[0].style.height = 'auto';
     }
     else if (children.length) {
       children[0].style.height = null;
@@ -81,6 +76,31 @@ const Box = React.createClass({
     this.setState({
       expanded: !this.state.expanded
     });
+  },
+  render: function() {
+    let anchor = '',
+        toggleClass = 'c-text__box',
+        readMore = null;
+    if (this.props.anchor_name) {
+      // Replace any spaces with _.
+      let anchor_name = util.cleanString(this.props.anchor_name);
+      anchor = <a name={anchor_name}></a>;
+    }
+    if (this.state.showToggle) {
+      toggleClass += this.state.expanded ? ' expanded' : ' collapsed';
+      let toggleText = this.state.expanded ? '-' : '+';
+      readMore = <div className="read-more"><a href="#" onClick={this._handleClick} className="toggle">{toggleText}</a></div>;
+    }
+    
+    return (
+      <div ref="box" className={toggleClass}>
+        {anchor}
+        <div className="inner">
+          {this.props.children}
+        </div>
+        {readMore}
+      </div>
+    );
   }
 });
 
