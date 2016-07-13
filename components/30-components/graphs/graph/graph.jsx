474d6d0b6443aa1f885309c441d7f44091c56300
pchop2 <pcho@phase2technology.com>
2016-07-13 14:20:44 -0400

    Always start graphs at 0 (#56)
    * Always start graphs at 0
    
    * Correctly applied padding

--- components/30-components/graphs/graph/graph.jsx
+++ components/30-components/graphs/graph/graph.jsx
@@ -65,6 +65,15 @@ export class BaseGraph extends Component {
         }
       }
 
+      // Always have the y axis start at 0
+      if (data.axis && data.axis.y) {
+        data.axis.y.padding = {
+          top: 50,
+          bottom: 0
+        };
+        data.axis.y.min = 0;
+      }
+
       // Relocate legend to top of the graph.
       if (!data.legend) {
         data.legend = {
