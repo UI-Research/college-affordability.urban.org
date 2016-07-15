f9d93d234e84a3e2895bc86f999ef0eeb60cf759
Peter Cho <pcho@phase2technology.com>
2016-07-15 15:07:28 -0400

    Tweaked viewport

--- components/10-html/html.jsx
+++ components/10-html/html.jsx
@@ -14,7 +14,7 @@ export default class HTML extends Component {
         <head>
           <meta charSet="utf-8" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-          <meta name="viewport" content="width=device-width; initial-scale=0.5, maximum-scale=0.5, minimum-scale=0.5; " />
+          <meta name="viewport" content="width=device-width; initial-scale=1;" />
           <title>{this.props.title} - Urban Institute</title>
         </head>
         <body>
