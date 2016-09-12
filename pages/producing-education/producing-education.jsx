cc2bfca6eff402691ca451155b9e3c3eeee7ce7a
hrecht <hrecht@users.noreply.github.com>
2016-09-12 15:22:38 -0400

    graphs for appropriations section test

--- pages/producing-education/producing-education.jsx
+++ pages/producing-education/producing-education.jsx
@@ -71,27 +71,23 @@ let singlepage = (
     <h3>Changes over time</h3>
     <p>Over the period from FY2000 to FY2014, enrollment in public colleges and universities grew by more than 30 percent. Meanwhile, in FY2014 overall state and local appropriations to public higher education fell below the FY2001 level. <strong>These two trends resulted in a 40 percent decline in appropriations per public-sector student between FY2000 and FY2014.</strong></p>
 
-    <Graph file={require('./json/single_bar_2.json')} />
-
-    <p><strong>In the nation as a whole, state and local appropriations per student have been on a downward trend since FY2000.</strong></p>
-
-    <Graph file={require('./json/single_bar.json')} />
+    <Graph file={require('./json/02_0010.json')} />
 
     <p>Because state and local governments fund public higher education using tax revenue, changes in appropriations tend to follow trends in tax revenues, which follow trends in the economy. The Great Recession dealt a blow to state budgets, leading states to reduce public funding for higher education. This funding would likely have declined more, had the federal government not supplemented state revenues with funds from the American Recovery and Reinvestment Act of 2009 (ARRA).</p>
     <p><strong>Yet now, even as tax revenues are increasing, appropriations continue to decline.</strong> Higher education appropriations declined from 5.9 percent of state and local tax revenues in FY2000 and 6.4 percent in FY2002 to 5.4 percent in FY2007 and 4.7 percent in FY2013.</p>
 
-    <Graph file={require('./json/single_bar_2.json')} />
+    <Graph file={require('./json/02_0020.json')} />
 
     <h3>Local Support</h3>
     <p><strong>While state support has been falling, local higher education support has been increasing.</strong> Still, in FY 2014, local support made up just 11 percent of state and local support for higher education</p>
 
-    <Graph file={require('./json/single_bar_2.json')} />
+    <Graph file={require('./json/02_0030.json')} />
 
     <h3>Variation Across States</h3>
     <p>National averages provide a meaningful overview of the circumstances facing college students, but state funding patterns and the prices students pay for college vary dramatically across states. <strong>Indeed, college affordability depends heavily on where students live.</strong></p>
     <p>In the 2014-15 school year, most states provided between $4,000 and $8,000 per student enrolled in a public college or university. However, New Hampshire provided just $2,632 per student, while Alaska provided $17,216. Some of the variation in per-student appropriations across states is attributable to differences in state wealth and costs of living, but some of the variation is the result of policy choices.</p>
 
-    <Graph file={require('./json/single_bar.json')} />
+    <Graph file={require('./json/02_0040.json')} />
 
 
     <h2>Endowments</h2>
