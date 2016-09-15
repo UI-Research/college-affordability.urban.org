34bf468cad54dde8ca6df053da074d2318d30db2
bchartoff <bchartoff@gmail.com>
2016-09-15 13:58:30 -0500

    flexible dual formats

--- helpers/formatting.jsx
+++ helpers/formatting.jsx
@@ -9,7 +9,12 @@ import util from 'util.jsx';
 
 module.exports = {
   f: (pattern, zero = false, type = 'axis', data = null) => {
+    // console.log(pattern)
     let alt;
+    let flag = "";
+    let scalar = 0;
+    // console.log("baz", pattern)
+
     if(pattern == 'dollar'){
         pattern = '$,.0f';
         alt = '$,.2s';
@@ -22,17 +27,36 @@ module.exports = {
         pattern = ',.0f';
         alt = ',.2s';
     }
-    else if(pattern.search('dual') == 0){
-        if(type == 'axis'){
-          pattern = '$,.0f';
-          alt = '$,.2s';
-        }else if(pattern == "dual_stacked"){
-          pattern = 0;
-          alt = "dual_stacked"
-        }else{
-          pattern = pattern.split("_")[1];
-          alt = "dual";
-        }
+    // else if(pattern.search('dual') == 0){
+    //     if(type == 'axis'){
+    //       pattern = '$,.0f';
+    //       alt = '$,.2s';
+    //     }else if(pattern == "dual_stacked"){
+    //       pattern = 0;
+    //       alt = "dual_stacked"
+    //     }else{
+    //       pattern = pattern.split("_")[1];
+    //       alt = "dual";
+    //     }
+    // }
+    else if(pattern.search("dual") == 0){
+      let components = pattern.split("_")
+      if (components[1].search("%") != -1){
+        flag = "dual_percent_first"
+        scalar = components[3]
+      }else{
+        flag = "dual_percent_second"
+      }
+      pattern = components[1]
+      alt = components[2]
+      console.log("dual", pattern, alt, flag, scalar)
+
+      // tmp = pattern
+      // pattern = tmp[0]
+      // alt = tmp[1]
+      // flag = "dual"
+      // console.log("bar")
+      // console.log(pattern, alt)
     }
 
     // Additional formatting logic.
@@ -53,9 +77,11 @@ module.exports = {
       if (_.isNumber(v) && v > 9999 && alt) {
         pattern = alt;
       }
-      if(alt == "dual"){
-        return d3.format('$,.0f')(v) + " (" + d3.format("%")(v/parseFloat(pattern)) + ")"
-      }else if(alt == "dual_stacked"){
+      else if(flag == "dual_percent_second"){
+        console.log("hello", data, type)
+        if(type == "axis"){
+          return d3.format(pattern)(v)
+        }
         var counter = (type == 'tooltip') ? j : i;
         var total = 0;
         for(var n = 0; n < data["data"]["columns"].length; n++){
@@ -64,11 +90,24 @@ module.exports = {
         if (v == 0 && type != 'tooltip') {
           return '';
         }else{
-          return d3.format('$,.0f')(v) + " (" + d3.format("%")(v/parseFloat(total)) + ")"
+          return d3.format(pattern)(v) + " (" + d3.format(alt)(v/parseFloat(total)) + ")"
         }
-
         
-      }else{
+      }
+      else if(flag == "dual_percent_first"){
+        console.log("hello", data, type)
+        if(type == "axis"){
+          return d3.format(pattern)(v)
+        }
+        if (v == 0 && type != 'tooltip') {
+          return '';
+        }else{
+          return d3.format(pattern)(v) + " (" + d3.format(alt)(v*parseFloat(scalar)) + ")"
+        }
+        
+      }
+
+      else{
         return d3.format(pattern)(v);
       }
     }
