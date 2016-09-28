08a474806cdd9bd7517113846ae1aad02af21301
hrecht <hrecht@users.noreply.github.com>
2016-09-28 13:15:28 -0400

    subsidies small multiples

--- pages/producing-education/producing-education.jsx
+++ pages/producing-education/producing-education.jsx
@@ -30,11 +30,19 @@ let singlepage = (
     <p><strong>Public</strong> research universities spend almost 50 percent more per student per year than public master’s universities and more than twice as much as public two-year colleges. These gaps have grown over time.</p>
     <p>Because the funds states provide to public institutions have not kept up with growing enrollments, students received smaller subsidies in 2013-14 than in 2005-06. This means <strong>that tuition and fees cover an increasing portion of educational expenses</strong>, as these prices have risen much more rapidly than total institutional expenditures.</p>
 
-    <Graph file={require('./json/test_groupedstacked.json')} />
+    <Multiples title="Education and Related Spending per Full-Time Equivalent Student at Public Institutions, 2005-–06 to 2013-–14" source="Integrated Postsecondary Education Data System" notes="Institutions are sorted according to the Carnegie Classification of Institutions of Higher Education framework.">
+      <Graph file={require('./json/02_0071.json')} small="true" />
+      <Graph file={require('./json/02_0072.json')} small="true" />
+      <Graph file={require('./json/02_0073.json')} small="true" />
+  </Multiples>
 
     <p>The pattern is quite different at <strong>private</strong> nonprofit colleges and universities, which are not dependent on state funding. Average per-student spending was two and a half times as high at private research universities as at public research universities, although the gap between private and public master’s universities was much smaller. Additionally, at private institutions, <strong>the share of spending covered by tuition revenues has remained constant</strong>.</p>
 
-    <Graph file={require('./json/grouped_bar.json')} />
+    <Multiples title="Education and Related Spending per Full-Time Equivalent Student at Private Nonprofit Institutions, 2005-–06 to 2013-–14" source="Integrated Postsecondary Education Data System" notes="Institutions are sorted according to the Carnegie Classification of Institutions of Higher Education framework.">
+      <Graph file={require('./json/02_0081.json')} small="true" />
+      <Graph file={require('./json/02_0082.json')} small="true" />
+      <Graph file={require('./json/02_0083.json')} small="true" />
+  </Multiples>
 
     <p>Average subsidies in each sector <strong>conceal wide variation across institutions, with a small number of colleges and universities in each sector providing a high percentage of total subsidies.</strong></p>
     <p>To examine within-sector variation, we broke students in the different sectors into deciles by the size of the subsidies their institutions gave them. This reveals that the institutions with the biggest subsidies skew the averages. <strong>The differences in per-student subsidy across types of institutions are less extreme when considering the middle 60 percent of undergraduate students.</strong></p>
