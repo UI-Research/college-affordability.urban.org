28ddb7ce25fa6ec94c3937138074961228a43e2f
Jerry Ta <jerryta@gmail.com>
2016-12-01 17:30:40 -0500

    Landing Pages Updated

--- pages/financial-aid/financial-aid.jsx
+++ pages/financial-aid/financial-aid.jsx
@@ -10,40 +10,22 @@ import util from 'util.jsx';
 
 let singlepage = (
   <div>
-    <h1>About</h1>
-
-    <h2>Overview</h2>
-    <p>Rising concerns over college tuition and student debt combined with the widespread conviction that a degree is essential for a middle-class lifestyle have led to a sense that college in the United States has become "unaffordable." <strong>But what does affordability actually mean?</strong></p>
-    <h3>Affordability depends on what you’re buying</h3>
-    <p>"College" means many different things, and whether college is "worth it" depends on what kind of institution and program you’re talking about. There are thousands of postsecondary institutions in the United States offering a wide range of undergraduate credentials, from short-term certificates in very specific occupational areas to associate degrees in either general academic fields or more targeted vocational areas to bachelor’s degrees—which also come in many different forms.</p>
-    <p>Just as someone who can afford to pay $20,000 for a new car cannot afford to pay nearly that much for a television, students should think of the price of a bachelor's degree quite differently than they think of the price of a short-term certificate. They should also consider the quality of the credential and the experiences of similar students. In addition, although we almost always focus on a year's tuition when talking about college prices, the number of years for which you pay that price matters a great deal. It is much more expensive to spend five years than four years in college to earn a bachelor’s degree.</p>
-    <h3>Sticker price is not an indicator of affordability</h3>
-    <p>Most students and families don't actually pay the whole cost of education. They attend institutions that charge less in tuition and fees than the amount they spend educating students, relying on funds from state and local tax revenues, endowment income, and other philanthropic sources to cover part of the costs. Moreover, many students receive grants and tax benefits that help them cover the tuition and fees that are charged.</p>
-    <p>On the flipside, students don't just have to pay tuition and fees. They also have to cover their living expenses while they are in college. What resources are available to pay all of these bills? Students can pay their share of the costs with a combination of income before college (including income earned by their parents), income earned during college, and income earned after college. They supplement these resources with financial aid from federal and state governments, colleges and universities, and other sources.</p>
-    <h3>Affordability is about more than what you can pay right now</h3>
-    <p>Having a sense of the value of the educational experience is critical to thinking about college affordability. The benefits of college are certainly not just in the form of future earnings. Students should expect to develop personally and intellectually, to open up lots of different kinds of opportunities, and to enjoy the process. But the money matters. Even if you have all the money in your pocket for four years of tuition, the education is unaffordable if it doesn’t do enough to increase your earnings potential.</p>
-    <p><strong><em>There is not a yes-or-no answer to the question of whether or not college is affordable.</em></strong>  But the information on this website can increase understanding of how much students in different circumstances pay for different kinds of education and of the resources they can draw on to cover their expenses. By presenting the big picture, trends over time, and the option to look more closely at the details, this website should support a more coherent and nuanced conversation about students’ financial access to a college education worth its cost.</p>
-
-    <h2>Credits</h2>
-    <strong>Research</strong>
-    <p>Sandy Baum and Martha Johnson</p>
-    <strong>Writing</strong>
-    <p>Alexandra Tisley</p>
-    <strong>Editorial</strong>
-    <p>Daniel Matos</p>
-
-    <h2>Funding</h2>
-    <p>Lumina Foundation provided funding for the development of this website. The views represented here are those of the authors and do not necessarily reflect the views of Lumina Foundation.</p>
+    <h1>Financial Aid</h1>
+      <p>The difference between a college&rsquo;s <a href="../prices-and-expenses">sticker price</a> and the net price a student pays is financial aid that does not have to be repaid. </p>
+      <p>Financial aid from a variety of sources helps students and families pay tuition and fees and the other expenses involved in getting a college education. Aid may come from the <a href="federal">federal government</a>, <a href="state">state governments</a>, <a href="other">employers, private organizations</a>, or <a href="institutional">colleges and universities</a>. </p>
+      <p>Some financial aid is distributed based on academic achievement, athletic ability, or other personal characteristics or accomplishments, but most aid is based on financial circumstances. The federal government has a formula for determining a family&rsquo;s ability to pay for college. Eligibility for federal grants, some federal loans, and much state grant aid is based on this formula. Colleges and universities may also use their own formulas for determining institutional aid.</p>
+      <p>Grants, scholarships, and tax credits or deductions all give students a tuition discount, decreasing the amount they have to pay for college. This type of financial aid makes college more affordable for many. </p>
+      <p>Although <a href="financial-need">loans</a> are often included in a financial aid package, they are not included in this section because, unlike grants or tax benefits, loans do not discount tuition. Loans do not reduce the net price, they simply allow students to postpone their payments—ultimately, students still have to foot the bills.</p>
   </div>
 );
 
 let specifications = {
-  'name': 'about',
-  'title': 'About Us',
+  'name': 'financial-aid',
+  'title': 'Financial Aid',
   'content': <SinglePage content={singlepage} />
 };
 
-const About = React.createClass({
+const FinancialAid = React.createClass({
   render: function() {
     return (
       <Template title={specifications.title} machineName={specifications.name} includeHTML={this.props.includeHTML}>
@@ -57,6 +39,6 @@ const About = React.createClass({
 if (util.canUseDOM()) {
   require('./' + specifications.name.toLowerCase() + '.scss');
 }
-util.printToPage('about', About);
+util.printToPage('financial-aid', FinancialAid);
 
-module.exports = About;
+module.exports = FinancialAid;
--- pages/what-is-college/what-is-college.jsx
+++ pages/what-is-college/what-is-college.jsx
@@ -11,14 +11,12 @@ import util from 'util.jsx';
 
 let singlepage = (
   <div>
-    <h1>What is College?</h1>
-    <h2>Overview</h2>
-    <p>Quisque nec orci at mi iaculis ornare vel eu enim. Fusce vestibulum cursus nisi, vestibulum rhoncus ligula porta non. Maecenas dignissim sagittis ante, at porta urna elementum vitae. Donec accumsan ac sapien vitae maximus. Integer malesuada auctor congue. Maecenas molestie efficitur risus ac ullamcorper. Proin orci leo, luctus vitae erat ac, tristique aliquam eros. Aliquam lobortis lorem ante, ut volutpat ante ornare ut. Vestibulum facilisis ullamcorper ornare. Praesent volutpat tincidunt felis, at convallis tortor elementum id.</p>
-    <p>Duis imperdiet posuere fermentum. Sed bibendum ultricies sapien sed placerat. Etiam ac nunc condimentum, ultrices velit a, malesuada nisi. Vestibulum risus justo, varius et orci ut, commodo convallis purus. Nulla velit urna, viverra eget accumsan quis, feugiat vitae erat. Pellentesque in porttitor orci. Suspendisse id risus et leo laoreet luctus sit amet et tellus. Donec urna nunc, dictum a quam a, ultrices dignissim nisl. Nulla lobortis cursus erat, id rhoncus quam mollis non. Nam ut ipsum dui.</p>
-    <h2>Heading 2</h2>
-    <p>Duis in diam sem. Nunc tempus malesuada nisl ac tincidunt. Donec eget rhoncus erat. Mauris finibus enim eget libero eleifend, sit amet pellentesque leo dignissim. Proin quis orci eu lectus iaculis iaculis. Mauris gravida vehicula eros, sit amet eleifend ex porttitor et. Ut eget tincidunt nisi.</p>
-    <h3>Sub Heading</h3>
-    <p>Donec ornare, sem id consectetur dapibus, tortor mi pretium metus, ac malesuada mauris ligula commodo massa. Phasellus sed aliquam risus. Curabitur id dui lacus. Sed eleifend dolor at pellentesque molestie. Maecenas fringilla sem condimentum, aliquam nisl sit amet, dignissim ex. Mauris mattis justo risus, ac sodales neque euismod quis. Duis porttitor eget risus ut dictum. Aenean laoreet elit elit, et pellentesque leo pellentesque eu. Duis sodales gravida lobortis. Suspendisse potenti. Vivamus urna nunc, viverra nec nulla sed, volutpat eleifend metus. Duis nec nisi a magna lobortis ultrices in id justo. Pellentesque vestibulum odio vehicula bibendum hendrerit. Maecenas dignissim lectus sapien, ut faucibus libero tempor sit amet. Aliquam erat volutpat.</p>
+    <h1>What is college?</h1>
+      <p>Drawing conclusions about &ldquo;college affordability&rdquo; is complicated by the many different types of colleges there are. Students pursue a variety of credentials that require different amounts of time and have different expected returns. Both the cost of higher education and whether it is financially feasible for students depend on the credentials they are pursuing and the institutions in which they enroll.</p>
+      <p>Media coverage of higher education tends to focus on the colleges on either end of the spectrum—those with billion-dollar endowments that regularly top rankings lists or those shutting down because of poor performance. But in reality, most students go to colleges that sit somewhere in between those two extremes. </p>
+      <p>Along that spectrum there are different kinds of <a href="institutions">institutions</a> and programs students can pick from. Colleges can be nonprofit or for profit, private or public, and doctoral or master&rsquo;s universities or bachelor&rsquo;s or associate colleges. Students can earn a bachelor&rsquo;s degree, an associate degree, or a certificate. </p>
+      <p>And just as colleges differ, so do <a href="students">students</a>. Though many students go to college straight out of high school, many go later in life. More than half of all undergraduates are age 23 or younger, but about a quarter are age 30 or older. Students come from different backgrounds, have access to different resources, and take different amounts of time to earn a degree.</p>
+      <p>To understand college affordability, it&rsquo;s important to first understand the different types of people trying to pay for college and the different colleges they have to choose from. </p>
   </div>
 );
 
