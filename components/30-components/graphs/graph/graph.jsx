308174bbcf0e2984539764a43d06923730c8011c
Ben Chartoff <bchartoff@gmail.com>
2017-01-06 13:38:06 -0500

    for long label multiples, force 2 per row layout

--- components/30-components/graphs/graph/graph.jsx
+++ components/30-components/graphs/graph/graph.jsx
@@ -14,6 +14,55 @@ if (util.canUseDOM()) {
   require('./graph.scss');
 }
 
+// https://tc39.github.io/ecma262/#sec-array.prototype.includes
+if (!Array.prototype.includes) {
+  Object.defineProperty(Array.prototype, 'includes', {
+    value: function(searchElement, fromIndex) {
+
+      // 1. Let O be ? ToObject(this value).
+      if (this == null) {
+        throw new TypeError('"this" is null or not defined');
+      }
+
+      var o = Object(this);
+
+      // 2. Let len be ? ToLength(? Get(O, "length")).
+      var len = o.length >>> 0;
+
+      // 3. If len is 0, return false.
+      if (len === 0) {
+        return false;
+      }
+
+      // 4. Let n be ? ToInteger(fromIndex).
+      //    (If fromIndex is undefined, this step produces the value 0.)
+      var n = fromIndex | 0;
+
+      // 5. If n ≥ 0, then
+      //  a. Let k be n.
+      // 6. Else n < 0,
+      //  a. Let k be len + n.
+      //  b. If k < 0, let k be 0.
+      var k = Math.max(n >= 0 ? n : len - Math.abs(n), 0);
+
+      // 7. Repeat, while k < len
+      while (k < len) {
+        // a. Let elementK be the result of ? Get(O, ! ToString(k)).
+        // b. If SameValueZero(searchElement, elementK) is true, return true.
+        // c. Increase k by 1.
+        // NOTE: === provides the correct "SameValueZero" comparison needed here.
+        if (o[k] === searchElement) {
+          return true;
+        }
+        k++;
+      }
+
+      // 8. Return false
+      return false;
+    }
+  });
+}
+
 // Keep graphing piece separate - need its own DOM interaction events.
 export class BaseGraph extends Component {
   constructor(props) {
@@ -25,6 +74,7 @@ export class BaseGraph extends Component {
 
       // Acquire graph data.
       let data = this.props.file;
+
 
       // Identify DOM element we want to apply the graph to.
       data.bindto = '#' + this.props.id;
@@ -295,7 +345,9 @@ export class BaseGraph extends Component {
       // apply special formatting for small graph sizes!
       if (this.props.small == 'true') {
         data.legend.show = false;
-        var width = (this.props.children == 2) ? 335 : 210;
+        var multExceptions = ["graph25","graph26","graph27","graph28","graph19", "graph20","graph21","graph4", "graph5","graph6"]
+
+        var width = (this.props.children == 2 || multExceptions.includes(this.props.id)) ? 335 : 210;
         data.padding = {"top": 10}
         if(data.data.type == "bar" && data.axis.rotated == true){
           data.padding = {"right" : 20, "bottom" : 20}
@@ -750,7 +802,10 @@ export class BaseGraph extends Component {
   barChartFormatting(object) {
     let bar_cats = d3.selectAll(`#${object.props.id}.c-bar__container .c3-axis-x[style*="visibility: visible"] .tick text`);
     let legend_cats = d3.selectAll(`#${object.props.id}_legend .c3-legend-item text`);
-
+    let multExceptions = ["graph25","graph26","graph27","graph28","graph19", "graph20","graph21","graph4", "graph5","graph6"]
+    if(multExceptions.includes(object.props.id)){
+      object.getClosest( d3.select(`#${object.props.id}`).node() , ".c-graph-multiple").classList.add("children_2")
+    }
     if(
       (object.getClosest( d3.select(`#${object.props.id}`).node() , ".c-graph").classList.contains("c-bar") 
       &&
