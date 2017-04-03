6ad2d4bd04d50fc10e5454b415feef6c195f7392
Ben Chartoff <bchartoff@gmail.com>
2017-04-03 15:36:08 -0500

    add social meta tags

--- components/10-html/html.jsx
+++ components/10-html/html.jsx
@@ -17,6 +17,18 @@ export default class HTML extends Component {
           <meta charSet="utf-8" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="twitter:card" content="summary">
+          <meta name="twitter:site" content="@urbaninstitute">
+          <meta name="twitter:creator" content="@urbaninstitute">
+          <meta name="twitter:title" content="Understanding College Affordability">
+          <meta name="twitter:description" content="Rising concerns over tuition and student debt have led to a sense that college has become \"unaffordable.\" But what does affordability actually mean?">
+          <meta name="twitter:image" content="http://collegeaffordability.urban.org/img/social.jpg">
+          <meta property="og:title" content="Understanding College Affordability" />
+          <meta property="og:description" content="Rising concerns over tuition and student debt have led to a sense that college has become \"unaffordable.\" But what does affordability actually mean?" />
+          <meta property="og:image" content="http://collegeaffordability.urban.org/img/social.jpg" />
+          <meta property="og:image:type" content="image/jpeg">
+          <meta property="og:url" content="http://urbn.is/2nPYxhg">
+
           <link href="/components.css" rel="stylesheet" />
           <title>{this.props.title} - Urban Institute</title>
         </head>
